c9ecfe85d1083c39f6aab91729873b2fe6145dd5
Nakshatr-ayo <95480625+morpheus-30@users.noreply.github.com>
2023-06-23 01:33:52 +0530

    Time: 8 ms (88.38%) | Memory: 8.2 MB (84.14%) - LeetSync

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-      if(s.size()==0)
-      return 0;
-  int maxans = INT_MIN;
-  unordered_set < int > set;
-  int l = 0;
-  for (int r = 0; r < s.length(); r++) // outer loop for traversing the string
-  {
-    if (set.find(s[r]) != set.end()) //if duplicate element is found
-    {
-      while (l < r && set.find(s[r]) != set.end()) {
-        set.erase(s[l]);
-        l++;
+      vector < int > mpp(256, -1);
+
+      int left = 0, right = 0;
+      int n = s.size();
+      int len = 0;
+      while (right < n) {
+        if (mpp[s[right]] != -1)
+          left = max(mpp[s[right]] + 1, left);
+
+        mpp[s[right]] = right;
+
+        len = max(len, right - left + 1);
+        right++;
       }
-    }
-    set.insert(s[r]);
-    maxans = max(maxans, r - l + 1);
-  }
-  return maxans;
+      return len;
     }
 };
