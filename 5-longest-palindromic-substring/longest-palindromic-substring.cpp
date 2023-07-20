9a4bd9d7370d59b0fe4414cf71ef56060e8c83a1
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2023-07-21 00:07:53 +0530

    Time: 1332 ms (11.67%) | Memory: 6.8 MB (97.26%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,29 +1,33 @@
 class Solution {
-bool check(string &s, int i, int j){
+public:
+
+    bool ifPalin(string &s,int i,int j){
         while(i<j){
-            if(s[i] != s[j]){
+            if(s[i]!=s[j]){
                 return false;
             }
             i++;
             j--;
         }
         return true;
-    }            
-public:
+    }
+
     string longestPalindrome(string s) {
         int n = s.size();
-        int starting_index = 0;
-        int max_len = 0;
-        for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
-                if(check(s, i, j)){
-                    if(j-i+1 > max_len){
-                        max_len = j-i+1;
-                        starting_index = i;
+        
+        int startin=0;
+        int maxL=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                // cout<<i<<" "<<j<<endl;
+                if(ifPalin(s,i,j)){
+                    if(maxL<j-i+1){
+                        startin = i;
+                        maxL=j-i+1;
                     }
                 }
             }
         }
-        return s.substr(starting_index, max_len);
+        return s.substr(startin,maxL);
     }
 };
