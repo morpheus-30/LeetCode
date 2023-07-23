887941dbd5cabbceeee6e6f2fd4bb32162e80b14
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2023-07-23 17:02:57 +0530

    Time: 20 ms (18.06%) | Memory: 7.3 MB (39.45%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,18 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        long s1 = 0;
-        long t1 = 0;
-        int i = s.size()-1;
-        for(auto x:s){
-            s1 = s1 + (x+x*x+x*x*x);
-            i--;
-        }
-        for(auto x:t){
-            t1 = t1 + (x+x*x+x*x*x);
-            i--;
-        }
-        cout<<t1<<" "<<s1;
-        return t1==s1;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
     }
 };
