b2cffe904f7618b6f1db54e8b84c6d5bcb19b3fa
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2023-09-16 14:36:36 +0530

    Time: 51 ms (75.63%) | Memory: 21.1 MB (67.36%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        if(grid[0][0] == 1)
+        if(grid[0][0]==1){
             return -1;
-        
-        int s = grid.size();
-        if(grid[s-1][s-1] != 0)
-            return -1;
-
-        queue<pair<pair<int, int>, int>> q;
-        q.push({{0, 0}, 1});
-        grid[0][0] = -1;
-
-        int drow[] = {-1, 0, +1, 0, 1, -1, -1, 1};
+        }
+        int n = grid.size();
+        if(grid[n-1][n-1] != 0){
+            return -1;}
+        // vector<vector<int>> dist(n,vector<int>(n,INT_MAX));
+        queue<pair<pair<int,int>,int>> q;
+        q.push({{0,0},1});
+        grid[0][0]=-1;
+        // dist[0][0] = 0;
+         int drow[] = {-1, 0, +1, 0, 1, -1, -1, 1};
         int dcol[] = {0, 1, 0, -1, 1, 1, -1, -1}; 
 
         while(!q.empty()){
-            auto val = q.front();
-            int i = val.first.first;
-            int j = val.first.second;
-            int path = val.second;
+
+            auto top = q.front();
+            int row = top.first.first;
+            int col = top.first.second;
+            int dis = top.second;
             q.pop();
-
-            if(i == s-1 && j == s-1)
-                return path;
-
-            for(int idx=0;idx<8;idx++){
-                int row = i + drow[idx];
-                int col = j + dcol[idx];
-
-                if(row >= 0 && row < s && col >= 0 && col < s && grid[row][col] == 0){
-                    grid[row][col] = -1;
-                    q.push({{row, col}, path+1});
-                }
+            if(row == n-1&&col == n-1){
+                return dis;
+            }
+            for(int i=0;i<8;i++){
+                
+                    int newR = row+drow[i];
+                    int newC = col+dcol[i];
+                    if(newR>=0&&newR<n&&newC>=0&&newC<n&&grid[newR][newC]==0){
+                  
+                        grid[newR][newC] = -1;
+                        q.push({{newR,newC},dis+1});
+                    }
+                
             }
         }
-
         return -1;
+
     }
 };
