a02d800c1e407a3be5a0d6656cc35cc1bae14918
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2023-10-14 19:02:56 +0530

    Time: 113 ms (13.57%) | Memory: 21.3 MB (26.73%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -1,40 +1,93 @@
-#include <algorithm>
-#include <vector>
 class Solution {
 public:
-int solve(vector<vector<char>>& matrix, int i , int j ,int& maxi,vector<vector<int>>&dp)
-{
-    // CHECK IF INDEX ARE OUT OF BOUND OR NOT 
-    if(i>=matrix.size() || j>=matrix[0].size())
-    {
-        return 0 ;
+
+    int solve(vector<vector<char>>& matrix,int i,int j,int &maxi){
+        int m = matrix.size();
+        int n = matrix[0].size();
+        if(i>=m||j>=n){
+            return 0;
+        }
+        int right = solve(matrix,i,j+1,maxi);
+        int down = solve(matrix,i+1,j,maxi);
+        int diag = solve(matrix,i+1,j+1,maxi);
+
+        if(matrix[i][j]=='1'){
+            int ans = 1+min(right,min(down,diag));
+            maxi = max(maxi,ans);
+            return ans;
+        }else{
+            return 0;
+        }
+    }
+    int solveMem(vector<vector<char>>& matrix,int i,int j,int &maxi,vector<vector<int>> &dp){
+        int m = matrix.size();
+        int n = matrix[0].size();
+        if(i>=m||j>=n){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int right = solveMem(matrix,i,j+1,maxi,dp);
+        int down = solveMem(matrix,i+1,j,maxi,dp);
+        int diag = solveMem(matrix,i+1,j+1,maxi,dp);
+
+        if(matrix[i][j]=='1'){
+            dp[i][j] = 1+min(right,min(down,diag));
+            maxi = max(maxi,dp[i][j]);
+            return dp[i][j];
+        }else{
+            return 0;
+        }
     }
 
-    if(dp[i][j]!=-1)
-    {
-        return dp[i][j];
-    }
-    int right=solve(matrix,i,j+1,maxi,dp);
-    int diagonal=solve(matrix,i+1,j+1,maxi,dp);
-    int bottom=solve(matrix,i+1,j,maxi,dp);
-    if(matrix[i][j]=='1')
-    {
-        dp[i][j] = 1+min(right, min(diagonal , bottom));
-        maxi=max(dp[i][j],maxi);
-        return dp[i][j];
-    }
-    else
-        return 0 ;
-
-}
-    int maximalSquare(vector<vector<char>>& matrix)
-    {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-
+    int maximalSquare(vector<vector<char>>& matrix) {
         int maxi = 0;
-        int s=solve(matrix, 0,0 , maxi,dp);
-          return maxi * maxi; 
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        solveMem(matrix,0,0,maxi,dp);
+        return maxi*maxi;
     }
 };
+
+// #include <algorithm>
+// #include <vector>
+// class Solution {
+// public:
+// int solve(vector<vector<char>>& matrix, int i , int j ,int& maxi,vector<vector<int>>&dp)
+// {
+//     // CHECK IF INDEX ARE OUT OF BOUND OR NOT 
+//     if(i>=matrix.size() || j>=matrix[0].size())
+//     {
+//         return 0 ;
+//     }
+
+//     if(dp[i][j]!=-1)
+//     {
+//         return dp[i][j];
+//     }
+//     int right=solve(matrix,i,j+1,maxi,dp);
+//     int diagonal=solve(matrix,i+1,j+1,maxi,dp);
+//     int bottom=solve(matrix,i+1,j,maxi,dp);
+//     if(matrix[i][j]=='1')
+//     {
+//         dp[i][j] = 1+min(right, min(diagonal , bottom));
+//         maxi=max(dp[i][j],maxi);
+//         return dp[i][j];
+//     }
+//     else
+//         return 0 ;
+
+// }
+//     int maximalSquare(vector<vector<char>>& matrix)
+//     {
+//         int n = matrix.size();
+//         int m = matrix[0].size();
+//         vector<vector<int>>dp(n,vector<int>(m,-1));
+
+//         int maxi = 0;
+//         int s=solve(matrix, 0,0 , maxi,dp);
+//           return maxi * maxi; 
+//     }
+// };
