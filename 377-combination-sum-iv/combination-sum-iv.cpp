701e928578d61c4bf21e37c89a6abc32c37c87e8
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2023-10-14 12:57:04 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (15.07%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -38,9 +38,22 @@ public:
         dp[target] = ans;
         return ans;
     }
+    int solveTab(vector<int> &nums,int target){
+        vector<unsigned> dp(target+1,0);
+        dp[0] = 1;
+        int n = nums.size();
+        for(int i=1;i<=target;i++){
+            for(int j=0;j<n;j++){
+                if((i-nums[j])>=0){
+                    dp[i]+=dp[i-nums[j]];
+                }
+            }
+        }
+        return dp[target];
+    }
 
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int> dp(target+1,-1);
-        return solveMem(nums,target,dp);
+        // vector<int> dp(target+1,-1);
+        return solveTab(nums,target);
     }
 };
