c2225bedb88d4582fba9f1f0b1cda2e5777874dd
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2023-10-14 13:33:39 +0530

    Time: 156 ms (54.51%) | Memory: 9.6 MB (43.90%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,3 +1,5 @@
+
+#include<bits/stdc++.h>
 class Solution {
 public:
 
@@ -20,16 +22,28 @@ public:
             return dp[n];
         }
         int ans = n;
-        
         for(int i=1;i*i<=n;i++){
             ans = min(ans,1+solveMem(n-i*i,dp));
         }
         dp[n] = ans;
         return dp[n];
     }
+    int solveTab(int n){
+        vector<int> dp(n+1,INT_MAX);
+        dp[0] = 0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j*j<=n;j++){
+                if(i-j*j>=0){
+                    // dp[i] = dp[i]>(1+dp[n-j*j])?(1+dp[i-j*j]):dp[i];
+                    dp[i] = min(dp[i],1+dp[i-j*j]);
+                }
+            }
+        }
+        return dp[n];
+    }
 
     int numSquares(int n) {
-        vector<int> dp(n+1,-1);
-        return solveMem(n,dp);
+        
+        return solveTab(n);
     }
 };
