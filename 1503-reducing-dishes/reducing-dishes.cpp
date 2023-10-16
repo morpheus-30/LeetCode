f0d1c81436c1e3700203ce0de6fb08c4f369b54e
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2023-10-17 00:02:33 +0530

    Time: 18 ms (57.53%) | Memory: 33.9 MB (7.75%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.cpp
+++ 1503-reducing-dishes/reducing-dishes.cpp
@@ -24,10 +24,22 @@ public:
         dp[index][time] = max(incl,excl);
         return dp[index][time];
     }
+    int solveTab(vector<int> &s){
+        vector<vector<int>> dp(s.size()+1,vector<int>(s.size()+1,0));
+        int n = s.size();
+        for(int i=n-1;i>=0;i--){
+            for(int t = i;t>=0;t--){
+                int incl = s[i]*(t+1) + dp[i+1][t+1];
+                int excl = dp[i+1][t];
+                dp[i][t] = max(incl,excl);
+            }
+        }
+        return dp[0][0];
+    }
 
     int maxSatisfaction(vector<int>& satisfaction) {
         sort(satisfaction.begin(),satisfaction.end());
         vector<vector<int>> dp(satisfaction.size()+1,vector<int>(satisfaction.size()+1,-1));
-        return solveMem(satisfaction,0,1,dp);
+        return solveTab(satisfaction);
     }
 };
