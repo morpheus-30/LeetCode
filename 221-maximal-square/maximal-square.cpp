889765a4dc3a629c1e217faf3e7631468b38d9cb
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2023-10-16 16:26:05 +0530

    Time: 67 ms (66.29%) | Memory: 23.3 MB (8.55%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -40,16 +40,33 @@ public:
             return 0;
         }
     }
-    // int solveTab(vector<vector<char>>& matrix,int i,int j,int &maxi,vector<vector<int>> &dp){
-        
-    // }
+    int solveTab(vector<vector<char>>& matrix,int &maxi){
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                int right = dp[i][j+1];
+                int down = dp[i+1][j];
+                int diag = dp[i+1][j+1];
+
+                if(matrix[i][j]=='1'){
+                    dp[i][j] = 1+min(right,min(down,diag));
+                    maxi = max(maxi,dp[i][j]);
+                }else{
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return dp[0][0];
+    }
 
     int maximalSquare(vector<vector<char>>& matrix) {
         int maxi = 0;
         int m = matrix.size();
         int n = matrix[0].size();
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        solveMem(matrix,0,0,maxi,dp);
+        solveTab(matrix,maxi);
         return maxi*maxi;
     }
 };
