922c3581e4a7da758d56ade845d908c875aaa37f
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2023-10-17 23:58:10 +0530

    Time: 247 ms (51.16%) | Memory: 95.4 MB (36.04%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+
+    bool solve(vector<int> &nums,int index,int target){
+        int n = nums.size();
+        if(target<0){
+            return 0;
+        }
+        if(index>=n){
+            return 0;
+        }
+        if(target==0){
+            return 1;
+        }
+        int incl = solve(nums,index+1,target-nums[index]);
+        int excl = solve(nums,index+1,target);
+        return incl or excl;
+
+    }
+    bool solveMem(vector<int> &nums,int index,int target,vector<vector<int>> &dp){
+        int n = nums.size();
+        if(target<0){
+            return 0;
+        }
+        if(index>=n){
+            return 0;
+        }
+        if(target==0){
+            return 1;
+        }
+        // for(int i=0;i<n;i++){
+        //     dp[i][0] = 1;
+        // }
+        if(dp[index][target]!=-1){
+            return dp[index][target];
+        }
+        int incl = solveMem(nums,index+1,target-nums[index],dp);
+        int excl = solveMem(nums,index+1,target,dp);
+        return dp[index][target] = (incl or excl);
+
+    }
+
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for(auto x:nums){
+            sum+=x;
+        }
+        if(sum%2!=0){
+            return false;
+        }
+        int n = nums.size();
+        vector<vector<int>> dp(n,vector<int>(sum/2+1,-1));
+        return solveMem(nums,0,sum/2,dp);
+    }
+};
