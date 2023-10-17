2f6d283efd1834c6577e5398f39fb442e949b563
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2023-10-17 19:31:44 +0530

    Time: 31 ms (37.66%) | Memory: 11 MB (8.71%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -43,9 +43,26 @@ public:
         }
         return dp[n][target] = ans;
     }
+    int solveTab(int n,int k,int target){
+        int mod = 1e9+7;
+        vector<vector<int>> dp(n+1,vector<int>(target+1,0));
+        dp[0][0] = 1;
+        for(int dice=1;dice<=n;dice++){
+            for(int t = 1;t<=target;t++){
+                int ans = 0;
+                for(int i=1;i<=k;i++){
+                    if(t-i>=0){
+                        ans = (ans+dp[dice-1][t-i])%mod; 
+                    }
+                }
+                dp[dice][t] = ans;
+            }
+        }
+        return dp[n][target];
+    }
 
     int numRollsToTarget(int n, int k, int target) {
         vector<vector<int>> dp(n+1,vector<int>(target+1,-1));
-        return solveMem(n,k,target,dp);
+        return solveTab(n,k,target);
     }
 };
