619acc35d7167e41a0f117fa7f30c14a4e4f5858
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2023-10-18 22:44:57 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (30.93%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
 
-    int solve(int n){
+    int solve(int n,vector<int> &dp){
         if(n<=1){
             return 1;
         }
         int ans = 0;
-        for(int i=1;i<=n;i++){
-            ans+=solve(i-1)*solve(n-i);
+        if(dp[n]!=-1){
+            return dp[n];
         }
-        return ans;
+        for(int i=1;i<=n;i++){
+            ans+=solve(i-1,dp)*solve(n-i,dp);
+        }
+        return dp[n] = ans;
     }
 
     int numTrees(int n) {
-        return solve(n);
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
     }
 };
