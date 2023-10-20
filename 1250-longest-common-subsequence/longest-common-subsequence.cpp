16b6b171217649afa0562ff228c3c38e490b5a9c
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2023-10-20 15:54:02 +0530

    Time: 51 ms (54.23%) | Memory: 24.9 MB (22.34%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
 
-    int solve(string text1, string text2,int i,int j,vector<vector<int>> &dp){
-        int m = text1.length();
-        int n = text2.length();
+    int solve(string &text1, string &text2,int i,int j,vector<vector<int>> &dp){
+        int m = text1.size();
+        int n = text2.size();
         if(i==m||j==n){
             return 0;
         }
@@ -18,19 +18,7 @@ public:
         }
         return dp[i][j] = ans;
     }
-    int topdown(string& text1,string& text2,int i,int j,vector<vector<int>>& dp){
-        if(i==text1.length()||j==text2.length())
-            return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int ans;
-        if(text1[i]==text2[j])
-            ans=1+topdown(text1,text2,i+1,j+1,dp);
-        else
-            ans=max(topdown(text1,text2,i,j+1,dp),topdown(text1,text2,i+1,j,dp));
-        dp[i][j]=ans;
-        return dp[i][j];
-    }
+    
     int solveTab(string text1, string text2){
         int m = text1.size();
         int n = text2.size();
@@ -51,6 +39,6 @@ public:
 
     int longestCommonSubsequence(string text1, string text2) {
         vector<vector<int>> dp(text1.size(),vector<int>(text2.size(),-1));
-        return topdown(text1,text2,0,0,dp);
+        return solve(text1,text2,0,0,dp);
     }
 };
