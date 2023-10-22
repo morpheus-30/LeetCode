afd42e2528b803fc3301bb097f026f3ebe562a6b
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2023-10-23 01:52:29 +0530

    Time: 15 ms (38.27%) | Memory: 11.7 MB (18.90%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -23,8 +23,36 @@ public:
         return dp[i1][i2]=ans;
     }
 
+    int solveTab(string &w1,string &w2){
+        vector<vector<int>> dp(w1.length()+1,vector<int>(w2.length()+1,0));
+
+        for(int i2 = 0;i2<w2.length();i2++){
+            dp[w1.length()][i2]= w2.size()-i2;
+        }
+        for(int i1 = 0;i1<w1.length();i1++){
+            dp[i1][w2.length()]= w1.size()-i1;
+        }
+
+
+        for(int i1 = w1.length()-1;i1>=0;i1--){
+            for(int i2 = w2.length()-1;i2>=0;i2--){
+                int ans = 0;
+                if(w1[i1]==w2[i2]){
+                    ans = dp[i1+1][i2+1];
+                }else{
+                    int insertAns = 1+dp[i1][i2+1];
+                    int delAns = 1+dp[i1+1][i2];
+                    int replAns = 1+dp[i1+1][i2+1];
+                    ans = min(insertAns,min(delAns,replAns));
+                }
+                dp[i1][i2]=ans;
+            }
+        }
+        return dp[0][0];
+    }
+
     int minDistance(string word1, string word2) {
         vector<vector<int>> dp(word1.length(),vector<int>(word2.length(),-1));
-        return solve(word1,word2,0,0,dp);
+        return solveTab(word1,word2);
     }
 };
