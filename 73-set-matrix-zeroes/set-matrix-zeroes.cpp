e32c4d3d2edf6b34acb60517e56a25e3abe6411b
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2023-10-29 11:11:17 +0530

    Time: 7 ms (91.70%) | Memory: 13.8 MB (55.79%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,26 +3,32 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
+        vector<int> hor(n,1);
+        vector<int> vert(m,1);
+
         for(int i=0;i<m;i++){
             for(int j = 0;j<n;j++){
                 if(matrix[i][j]==0){
-                    for(int k = 0;k<max(m,n);k++){
-                        if(k<m&&matrix[k][j]!=0){
-                            matrix[k][j] = -69;
-                        }
-                        if(k<n&&matrix[i][k]!=0){
-                            matrix[i][k] = -69;
-                        }
-                    }
+                    vert[i] = 0;
+                    hor[j] = 0;
                 }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if(matrix[i][j]==-69){
-                    matrix[i][j]=0;
+        
+        for(int i = 0;i<n;i++){
+            if(hor[i]==0){
+                for(int k = 0;k<m;k++){
+                    matrix[k][i] = 0;
                 }
             }
         }
+        for(int i = 0;i<m;i++){
+            if(vert[i]==0){
+                for(int k = 0;k<n;k++){
+                    matrix[i][k] = 0;
+                }
+            }
+        }
+        
     }
 };
