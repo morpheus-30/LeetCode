9127152a62bd518ee6887e3de30054ec155b5a6f
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2023-11-11 13:00:32 +0530

    Time: 152 ms (85.47%) | Memory: 124.5 MB (46.73%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,40 +1,21 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> ans;
         int n = nums.size();
-        int pos = -1,neg =-1;
-        bool flagpos = false;
-        bool flagneg = false;
-        for(int i=0;i<n;i++){
-            if(nums[i]>=0&&flagpos==false){
-                pos = i;
-                flagpos = true;
-            }else if(nums[i]<0&&flagneg==false){
-                neg = i;
-                flagneg = true;
-            }else if(flagpos==true&&flagneg==true){
-                break;
+        vector<int> ans(n,-1);
+        int pos = 0;
+        int neg = 1;
+        for(auto x:nums){
+            if(x>0){
+                ans[pos] = x;
+                pos+=2;
             }
-        }
-        while(pos<n&&neg<n){
-            ans.push_back(nums[pos]);
-            ans.push_back(nums[neg]);
-            pos++;
-            neg++;
-            while(neg<n&&nums[neg]>=0){
-                neg++;
+            if(x<0){
+                ans[neg] = x;
+                neg+=2;
             }
-            while(pos<n&&nums[pos]<0){
-                pos++;
-            }
-        }
-        while(neg<n){
-            ans.push_back(nums[neg++]);
-        }
-        while(pos<n){
-            ans.push_back(nums[pos++]);
         }
         return ans;
+
     }
 };
