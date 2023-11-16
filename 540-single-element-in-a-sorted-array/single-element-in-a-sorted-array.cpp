3004c6dec234688e94241f937bbb8f9b515e7ff5
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2023-11-16 13:01:59 +0530

    Time: 23 ms (30.78%) | Memory: 22.7 MB (53.51%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,10 +1,28 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int xori = 0;
-        for(auto x:nums){
-            xori = xori^x;
+        int low = 1;
+        int n = nums.size();
+        int high = n-2;
+         if (n == 1) return nums[0];
+    if (nums[0] != nums[1]) return nums[0];
+    if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid-1]!=nums[mid]&&nums[mid]!=nums[mid+1]){
+                return nums[mid];
+            }
+            if ((mid % 2 == 1 && nums[mid] == nums[mid - 1])
+                    || (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
+                //eliminate the left half:
+                low = mid + 1;
+            }
+            //we are in the right:
+            else {
+                //eliminate the right half:
+                high = mid - 1;
+            }
         }
-        return xori;
+        return -1;
     }
 };
