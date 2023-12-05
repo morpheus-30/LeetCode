0cac5e54e34030b7007e045e116978119fc4e6b3
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2023-12-05 13:27:03 +0530

    Time: 2 ms (67.58%) | Memory: 7.1 MB (41.24%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        string ans = "";
-        int count = 0;
-        string currComp = "";
-        for(auto x:s){
-            if(x=='('){
-                count++;
-            }else{
-                count--;
-            }
-            currComp+=x;
-            if(count==0){
-                string comp = currComp.substr(1,currComp.size()-2);
-                ans+=comp;
-                currComp = "";
-            }
+        string res;
+        int opened = 0;
+        for (char c : s) {
+            if (c == '(' && opened++ > 0) res += c;
+            if (c == ')' && opened-- > 1) res += c;
         }
-        return ans;
+        return res;
 
     }
 };
