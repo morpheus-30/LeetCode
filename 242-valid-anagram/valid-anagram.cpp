524786d304d31bc0abeb201777d134b0378674ca
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2023-12-06 14:15:52 +0530

    Time: 11 ms (44.97%) | Memory: 7.7 MB (79.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,23 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size()){
-            return false;
-        }
-        int n = s.size();
-        unordered_map<char,int> mp1;
-        unordered_map<char,int> mp2;
-        for(int i=0;i<n;i++){
-            mp1[s[i]]++;
-            mp2[t[i]]++;
-        }
-        for(auto x:mp1){
-            if(mp1[x.first]!=mp2[x.first]){
-                return false;
-            }
-        }
-        return true;
-
-
-    }
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        // cout<<s<<" "<<t;
+        return s==t;
+    }   
 };
