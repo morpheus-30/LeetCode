4da4e28de106e5155ed12eb9ed9744e99953f0aa
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2023-12-06 14:08:53 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (26.17%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,37 +1,12 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        //step1 find number of rotations
-        //step2 compare the whole string
-        if(s.size()!=goal.size()){
+        if (s.length() != goal.length()) {
             return false;
         }
-        bool flag = false;
-        goal = goal+goal;
-        int lastJ = -1;
-        int i = 0;
-        int j = 0;
-        int n = s.size();
-        while(i<n&&j<2*n){
-            if(s[i]==goal[j]){
-                if(flag==false){
-                    flag = true;
-                    lastJ = j;
-                }
-                i++;
-                j++;
-            }else{
-                if(flag==true){
-                    j = lastJ;
-                    flag = false;
-                }
-                j++;
-                i=0;
-            }
-        }
-        if(i==n){
-            return true;
-        }
-        return false;
+
+        string concatenated = goal + goal;
+
+        return concatenated.find(s) != string::npos;
     }
 };
