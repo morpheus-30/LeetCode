a476f0eb34cbcdb830d74368af3c0a1844b8c014
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2023-12-10 18:30:58 +0530

    Time: 3 ms (44.31%) | Memory: 6.6 MB (56.02%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -3,7 +3,7 @@ public:
     int maxDepth(string s) {
         int count = 0;
         int maxCount = 0;
-        for(auto x:s){
+        for(auto &x:s){
             if(x=='('){
                 count++;
             }else if(x==')'){
