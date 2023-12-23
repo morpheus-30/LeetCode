3c5a4a3fbca17d9be3d7a3404aea8449ac5e89d3
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2023-12-23 14:54:26 +0530

    Time: 3 ms (41.97%) | Memory: 6.3 MB (60.05%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
-
-    double solve(double x,long long n){
-        if(x==1.0000000000001&&n==-2147483648){
-            return 0.99979;
-            
-        }
-        if(x==1||x==-1||x==0){
+    double myPow(double x, int n) {
+        if(x==1||x==0||x==-1){
             if(x==1||x==0){
                 return x;
             }else{
                 return n%2==0?1:-1;
             }
         }
-        if(n==INT_MAX||n==INT_MIN){
-            return 0;
+        long long N = n;
+        if(N==0){
+            return 1;
         }
-        // cout<<x<<endl;
-        if(n==1){
+        if(N==1){
             return x;
         }
-        if(n>0){
-            return x*solve(x,n-1);
-        }else{
-            return 1/x*solve(x,n+1);
+        if(N<0){
+            x=1/x;
+            N = -N;
         }
-    }
+        if(N%2==0){
+            return myPow(x*x,N/2);
+        }else{
+            return x*myPow(x*x,N/2);
+        }
 
-    double myPow(double x, int n) {
-        long long nn = n;
-        return solve(x,nn);
-        // return 0;
     }
 };
