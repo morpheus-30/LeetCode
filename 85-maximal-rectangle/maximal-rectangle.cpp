253e702dcff100f9fba3141f4f6457cbb5b3c845
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2024-01-18 15:46:41 +0530

    Time: 79 ms (15.60%) | Memory: 14.4 MB (57.59%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -2,77 +2,45 @@
 using namespace std;
 class Solution {
 public:
-    
-     vector<int> getSmallerIndexLeft(vector<int> v){
+    int largestRectangleArea(vector<int>& histo) {
         stack<int> st;
-        int n = v.size();
-        vector<int> toBeReturned(n+1);
-        for(int i = 0;i<n;i++){
-            while(!st.empty()&&v[st.top()]>=v[i]){
+        int maxA = 0;
+        int n = histo.size();
+        for (int i = 0; i <= n; i++) {
+            while (!st.empty() && (i == n || histo[st.top()] >= histo[i])) {
+                int height = histo[st.top()];
                 st.pop();
-            }
-            if(st.empty()){
-                toBeReturned[i] = -1;
-            }else{
-                toBeReturned[i] = st.top();
+                int width;
+                if (st.empty())
+                    width = i;
+                else
+                    width = i - st.top() - 1;
+                maxA = max(maxA, width * height);
             }
             st.push(i);
         }
-        return toBeReturned;
-    }
-    vector<int> getSmallerIndexRight(vector<int> v){
-        stack<int> st;
-        int n = v.size();
-        vector<int> toBeReturned(n+1);
-        for(int i = n-1;i>=0;i--){
-            while(!st.empty()&&v[st.top()]>=v[i]){
-                st.pop();
-            }
-            if(st.empty()){
-                toBeReturned[i] = n;
-            }else{
-                toBeReturned[i] = st.top();
-            }
-            st.push(i);
-        }
-        return toBeReturned;
-    }
-
-    int largestRectangleArea(vector<int>& heights) {
-        if(heights.size()==1){
-            return heights[0];
-        }
-        vector<int> left = getSmallerIndexLeft(heights);
-        vector<int> right = getSmallerIndexRight(heights);
-        int maxi = 0;
-        int n = heights.size();
-        for(int i=0;i<n;i++){
-            int maxArea = (right[i]-left[i]-1)*heights[i];
-            maxi = max(maxi,maxArea);
-        }
-        return maxi;
-        
+        return maxA;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<int> oldTempV(m,0);
+        vector<int> oldTempV(m, 0);
         int maxArea = 0;
-        for(int i=0;i<n;i++){
-            vector<int> tempv(m,0);
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]!='0'){
-                    int matrixChar = matrix[i][j]-'0';
-                    tempv[j] = matrixChar+oldTempV[j];
+        for (int i = 0; i < n; i++) {
+            vector<int> tempv(m, 0);
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] != '0') {
+                    int matrixChar = matrix[i][j] - '0';
+                    tempv[j] = matrixChar + oldTempV[j];
                 }
             }
-            for(auto x:tempv){
-                cout<<x<<" ";
+            for (auto x : tempv) {
+                cout << x << " ";
             }
-            cout<<endl;
+            cout << endl;
             oldTempV = tempv;
             int area = largestRectangleArea(tempv);
-            maxArea = max(maxArea,area);
+            maxArea = max(maxArea, area);
         }
         return maxArea;
     }
