e164cd0b8ef382eec91953278f4296b83bd39d9d
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2024-01-31 16:00:24 +0530

    Time: 98 ms (28.76%) | Memory: 52.7 MB (7.50%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,38 +1,42 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        priority_queue<int> pq;
-        vector<int>mp(26,0);
 
-        for(char i:tasks){
-            mp[i-'A']++;  // count the number of times a task needs to be done
-        }   
-        for(int i=0;i<26;++i){
-            if(mp[i]) 
-            pq.push(mp[i]);
+        if(n==0) return tasks.size();
+
+        priority_queue<pair<int,char>> pq;
+        unordered_map<int,int> freq;
+
+        for(int i = 0;i<tasks.size();i++)
+        freq[tasks[i]]++;
+
+        for(auto x : freq)
+        pq.push({x.second,x.first});
+        
+        int ans = 0;
+        while(pq.size()>0)
+        {
+           if(pq.top().first==1)
+           {
+            ans+=pq.size();
+            break;
+           }
+           vector<pair<int,char>> v;
+           v.push_back(pq.top());
+           pq.pop();
+           ans+=1;
+           for(int i = 0;i<n;i++)
+           {
+               if(pq.size()>0)
+               {
+                 v.push_back(pq.top());
+                 pq.pop();
+               }
+               ans++;
+           }
+           for(auto x : v)
+           if(x.first>1) pq.push({x.first-1,x.second});
         }
-
-        int time=0; // stores the total time taken 
-        while(!pq.empty()){
-            vector<int>remain;
-            int cycle=n+1;  // n+1 is the CPU cycle length, if n is the cooldown period then after a task A there will be n more tasks. Hence n+1.
-
-            while(cycle and !pq.empty()){
-                int max_freq=pq.top(); // the task at the top should be first assigned the CPU as it has highest frequency
-                pq.pop();
-                if(max_freq>1){ // task with more than one occurrence, the next occurrence will be done in the next cycle 
-                    remain.push_back(max_freq-1); // add it to remaining task list
-                }
-                ++time; 
-                --cycle; 
-            }
-
-            for(int count:remain){
-                pq.push(count); 
-            }
-            if(pq.empty())break; // if the priority queue is empty than all tasks are completed and we don't need to include the idle time
-            time+=cycle; // this counts the idle time 
-        }
-        return time;
+        return ans; 
     }
 };
