c4bd7c87a2212549370d877e0c54331ab12b103d
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2024-01-31 19:53:00 +0530

    Time: 26 ms (69.96%) | Memory: 23.5 MB (10.14%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
@@ -1,51 +1,25 @@
-class treeNode {
-       public:
-      int val;
-      treeNode *left;
-      treeNode *right;
-      treeNode() : val(0), left(nullptr), right(nullptr) {}
-      treeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-      treeNode(int x, treeNode *left, treeNode *right) : val(x), left(left), right(right) {}
- };
-
-int KthLargest2(treeNode* root, int&k) {
-        if( root == NULL) return -1;
-
-        int left_ans = KthLargest2(root->right,k);
-        if(left_ans != -1) return left_ans;
-        k--;
-        if(k == 0) return root->val;
-        return  KthLargest2(root->left,k);
-    }
-  treeNode* insertIntoBST(treeNode* root, int val) {
-        if(root == NULL){
-            treeNode*newnode = new treeNode(val);
-            return newnode;
-        }
-        if(val < root->val) root->left = insertIntoBST(root->left,val);
-        else root->right = insertIntoBST(root->right , val); 
-        return root;
-    }
-
 class KthLargest {
 public:
-    int k ;
-    priority_queue<int>pq;
-    treeNode*root = NULL;
+priority_queue<int> q;
+int size;
     KthLargest(int k, vector<int>& nums) {
-        this->k = k;
-        for(auto i : nums)
-            root = insertIntoBST(root,i);
+        size = k;
+        for(int i : nums) add(i);
     }
-   
+    
     int add(int val) {
-        treeNode*temp = root;
-        root  = insertIntoBST(temp,val);
-        int s = k;
-        return KthLargest2(root,s);
+        if(q.size()<size) {
+            q.push(-val);
+        }
+        else {
+            if(-q.top()<val) {
+                q.pop();
+                q.push(-val);
+            }
+        }
+        return -q.top();
     }
 };
-
 /**
  * Your KthLargest object will be instantiated and called as such:
  * KthLargest* obj = new KthLargest(k, nums);
