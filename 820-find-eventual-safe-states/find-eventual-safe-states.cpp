d4120b02d6d1198b3f58ee0a50f75dc56a9322ad
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2024-04-16 21:01:29 +0530

    Time: 164 ms (14.51%) | Memory: 65.1 MB (33.73%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,52 +1,46 @@
+//Use Kahn's Algorithm
 class Solution {
 public:
+    int n; 
+    vector<int> indeg;
+    vector<vector<int>> adj;
 
-    bool dfs(int src,vector<int> adj[],vector<int> &visited,vector<int> &pathVisited,vector<int> &check){
-        visited[src] = 1;
-        pathVisited[src] = 1;
-        check[src] = 0;
-        for(auto x:adj[src]){
-            if(!visited[x]){
-                if(dfs(x,adj,visited,pathVisited,check)){
-                    check[src] = 0;
-                    return true;
-                }
-            }else if(pathVisited[x]){
-                check[src] = 0;
-                return true;
+    void adj_ini(vector<vector<int>>& graph){
+        for(int i=0; i<n ; i++){
+            for(int j=graph[i].size()-1; j>=0; j--){
+                int v=graph[i][j];
+                adj[v].push_back(i);
+                indeg[i]++;
             }
-            
         }
-        check[src] = 1;
-        pathVisited[src] = 0;
-        return false;
     }
-    
-    // Function to detect cycle in a directed graph.
 
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int V = graph.size();
-        vector<int> adj[V];
-      
-           for(int i=0;i<V;i++){
-            for(auto it : graph[i]){
-                int x = it;
-                adj[i].push_back(x);
+        n=graph.size();
+        indeg.assign(n, 0);
+        adj.resize(n);
+        adj_ini(graph);
+        queue<int> q;
+
+        for(int i=n-1; i>=0; i--)
+            if (indeg[i]==0) q.push(i);
+
+        vector<bool> safe(n, 0);
+        while(!q.empty()){
+            int x=q.front();
+        //    cout<<x<<"->";;
+            q.pop();
+            safe[x]=1;
+            for(int y: adj[x]){
+        //        cout<<y<<",";
+                if (--indeg[y]==0)
+                    q.push(y);
             }
+        //    cout<<endl;
         }
-        vector<int> check(V,0);
-        vector<int> visited(V,0);
-        vector<int> pathVisited(V,0);
         vector<int> ans;
-        for(int i=0;i<V;i++){
-            if(!visited[i]){
-                dfs(i,adj,visited,pathVisited,check);
-            }
-        }
-        for (int i = 0; i < V; i++) {
-			if (check[i] == 1) ans.push_back(i);
-		}
+        for(int i=0; i<n; i++)
+            if (safe[i]) ans.push_back(i);
         return ans;
-        
     }
 };
