534803a9c3e5ef050562d242c97386eb61842115
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2024-06-23 18:20:41 +0530

    Time: 119 ms (66.93%) | Memory: 61.5 MB (9.01%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -1,24 +1,7 @@
 class Solution {
 public:
-    int maxDistance(vector<int>& position, int m) {
-        sort(position.begin(), position.end());
-        int lo = 1;
-        int hi = (position.back() - position[0]) / (m - 1);
-        int ans = 1;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (canWePlace(position, mid, m)) {
-                ans = mid;
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
-            }
-        }
-        return ans;
-    }
-
-private:
-    bool canWePlace(const vector<int>& arr, int dist, int balls) {
+    
+bool canIfit(const vector<int>& arr, int dist, int balls) {
         int countBalls = 1;
         int lastPlaced = arr[0];
         for (int i = 1; i < arr.size(); i++) {
@@ -32,4 +15,23 @@ private:
         }
         return false;
     }
+
+int maxDistance(vector<int>& position, int m) {
+	sort(position.begin(),position.end());
+	int low = 1;
+	int n = position.size();
+	int high = position[n-1]-position[0]/m-1;
+	int ans = 1;
+	while(low<=high){
+		int mid = (low+high)/2;
+		if(canIfit(position,mid,m)){
+			ans = mid;
+			low = mid+1;
+		}else{
+			high = mid-1;
+		}
+	}
+	return ans;
+}
+
 };
