7b094cab28ea82fe58f513e19b4340e1c7f7b908
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2024-06-24 00:24:25 +0530

    Time: 44 ms (15.33%) | Memory: 25.9 MB (8.88%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,44 +1,27 @@
 class Solution {
 public:
+int solve(string&a,string&b,int i,int j,vector<vector<int>>&dp){
+    //base case
+    if(i==a.size())
+    return 0;
 
-    int solve(string &text1, string &text2,int i,int j,vector<vector<int>> &dp){
-        int m = text1.size();
-        int n = text2.size();
-        if(i==m||j==n){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int ans = 0;
-        if(text1[i]==text2[j]){
-            ans = 1+solve(text1,text2,i+1,j+1,dp);
-        }else{
-            ans = max(solve(text1,text2,i+1,j,dp),solve(text1,text2,i,j+1,dp));
-        }
-        return dp[i][j] = ans;
-    }
-    
-    int solveTab(string text1, string text2){
-        int m = text1.size();
-        int n = text2.size();
-         vector<vector<int>> dp(text1.size()+1,vector<int>(text2.size()+1,0));
-        for(int i=m-1;i>=0;i--){
-            for(int j = n-1;j>=0;j--){
-                int ans = 0;
-                if(text1[i]==text2[j]){
-                    ans = 1+dp[i+1][j+1];
-                }else{
-                    ans = max(dp[i+1][j],dp[i][j+1]);
-                }
-                dp[i][j] = ans;
-            }
-        }
-        return dp[0][0];
-    }
-
+if(j==b.size())
+    return 0;
+if(dp[i][j]!=-1)
+return dp[i][j];
+int ans=0;
+if(a[i]==b[j]){
+    ans=1+solve(a,b,i+1,j+1,dp);
+}
+else{
+    ans=max(solve(a,b,i+1,j,dp),solve(a,b,i,j+1,dp));
+}
+dp[i][j]=ans;
+return dp[i][j];
+}
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>> dp(text1.size(),vector<int>(text2.size(),-1));
-        return solve(text1,text2,0,0,dp);
+        vector<vector<int>>dp(text1.size(),vector<int>(text2.size(),-1));
+   return solve(text1,text2,0,0,dp);
+        
     }
 };
