7568559220724b3f3dc16316177d969c74bc1b75
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2024-06-27 15:06:58 +0530

    Time: 533 ms (11.76%) | Memory: 294.6 MB (20.21%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,72 +1,26 @@
 class Solution {
 public:
 
-    int solve(vector<int> nums,int currIndex,int prevIndex){
-        int n = nums.size();
-        if(currIndex==n){
-            return 0;            
-        }
-        int take = 0;
-        if(prevIndex==-1||nums[currIndex]>nums[prevIndex]){
-            take = 1+solve(nums,currIndex+1,currIndex);
-        }
-        int notTake = solve(nums,currIndex+1,prevIndex);
-        return max(take,notTake);
-    }
-    int solveMem(vector<int> &nums,int currIndex,int prevIndex,vector<vector<int>> &dp){
-        int n = nums.size();
-        if(currIndex==n){
-            return 0;            
-        }
-        if(dp[currIndex][prevIndex+1]!=-1){
-            return dp[currIndex][prevIndex+1];
-        }
-        int take = 0;
-        if(prevIndex==-1||nums[currIndex]>nums[prevIndex]){
-            take = 1+solveMem(nums,currIndex+1,currIndex,dp);
-        }
-        int notTake = solveMem(nums,currIndex+1,prevIndex,dp);
-        dp[currIndex][prevIndex+1] = max(take,notTake);
-        return dp[currIndex][prevIndex+1];
-    }
 
-    int solveTab(vector<int> &nums){
-        int n = nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-        for(int currIndex = n-1;currIndex>=0;currIndex--){
-            for(int prevIndex = currIndex-1;prevIndex>=-1;prevIndex--){
-                int take = 0;
-                if(prevIndex==-1||nums[currIndex]>nums[prevIndex]){
-                    take = 1+dp[currIndex+1][currIndex+1];
-                }
-                int notTake = dp[currIndex+1][prevIndex+1];
-                dp[currIndex][prevIndex+1] = max(take,notTake);
-            }
-        }
-        return dp[0][0]; 
-    }
-
-    int solveOptimal(vector<int> &nums){
-        int n = nums.size();
-        if(n==0){
+    int solve(vector<int>&nums,int index,int lastIndex,vector<vector<int>> &dp){
+        if(index<0){
             return 0;
         }
-        vector<int> ans;
-        ans.push_back(nums[0]);
-        for(int i=1;i<n;i++){
-            if(nums[i]>ans.back()){
-                ans.push_back(nums[i]);
-            }else{
-                int index = lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
-                ans[index] = nums[i];
-            }
+        if(dp[index][lastIndex+1]!=-1){
+            return dp[index][lastIndex+1];
         }
-        return ans.size();
+        int take = 0;
+        if(lastIndex==-1||nums[lastIndex]<nums[index]){
+            take = 1+solve(nums,index-1,index,dp);
+        }
+        int nottake = solve(nums,index-1,lastIndex,dp);
+        return dp[index][lastIndex+1] = max(take,nottake);
     }
 
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        return solveOptimal(nums);
+        reverse(nums.begin(),nums.end());
+        int n = nums.size()-1;
+        vector<vector<int>> dp(n+1,vector<int>(n+2,-1));
+        return solve(nums,n,-1,dp);
     }
 };
