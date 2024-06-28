6c6988a83b5761b974bffcccfa5c62d59ba5b8ad
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2024-06-28 12:49:38 +0530

    Time: 252 ms (81.55%) | Memory: 115.2 MB (99.66%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
@@ -1,51 +1,24 @@
 class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
-        vector<int> assign(n,-1);
-        vector<int> visited(n,-1);
-        vector<int> adj[n];
-        for(auto x:roads){
-        	adj[x[0]].push_back(x[1]);
-        	adj[x[1]].push_back(x[0]);
+        int occurrence[n];
+        for(int i = 0; i < n; i++)
+        {
+            occurrence[i] = 0;
         }
-        int root = 0;
-
-        // for(int i=1;i<n;i++){
-        // 	if(adj[i].size()>adj[root].size()){
-        // 		root = i;
-        // 	}
-        // }
-        int nextVal = n;
-        priority_queue<pair<int,int>> q;
-        for(int i=0;i<n;i++){
-            q.push({adj[i].size(),i});
+        for(int i = 0; i < roads.size(); i++)
+        {
+            occurrence[roads[i][0]]++;
+            occurrence[roads[i][1]]++;
         }
-        // q.push({adj[root].size(),root});
-        while(!q.empty()){
-        	auto front = q.top();
-        	q.pop();
+        sort(occurrence, occurrence + n, greater<int>());
 
-
-        	int r = front.first;
-        	int node = front.second;
-            cout<<nextVal<<" "<<node<<endl;
-            if(assign[node]==-1){
-                assign[node] = nextVal;
-                nextVal--;
-            }
-
-        	for(auto x:adj[node]){
-        		if(visited[x]==-1){
-                    visited[x] = 1;
-        			q.push({adj[x].size(),x});
-        		}
-        	}
+        // Step 3: Calculate the maximum importance
+        long long maxImportance = 0;
+        for (int i = 0; i < n; i++) {
+            maxImportance += (long long)occurrence[i] * (n - i);
         }
-        
-        long long ans = 0;
-        for(auto x:roads){
-            ans+=(assign[x[0]]+assign[x[1]]);
-        }
-        return ans;
+
+        return maxImportance;
     }
 };
