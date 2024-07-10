c950950d1d47f115356bbb0425797ba515ae256e
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2024-07-10 17:00:21 +0530

    Time: 6 ms (39.62%) | Memory: 7.5 MB (36.99%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        std::vector<int> circle;
-        for (int i = 1; i <= n; ++i) {
-            circle.push_back(i);
+      
+        vector<int> friends;
+        for(int i =1;i<=n;i++){
+            friends.push_back(i);
         }
-        int cur_ind = 0;
-
-        while (circle.size() > 1) {
-            int next_to_remove = (cur_ind + k - 1) % circle.size();
-            circle.erase(circle.begin() + next_to_remove);
-            cur_ind = next_to_remove;
+        int curr = 0;
+        while(friends.size()>1){
+            int remIndex = (curr+k-1)%friends.size();
+            cout<<friends[remIndex]<<endl;
+            curr = (remIndex);
+            friends.erase(friends.begin()+remIndex);
+            // if(curr = -1){
+            //     curr = friends.size()-1;
+            // }
         }
-
-        return circle[0];
+        return friends[0];
     }
 };
