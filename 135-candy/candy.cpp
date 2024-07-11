10b62b558736440640090b44956ba81570710b3d
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2024-07-11 14:38:19 +0530

    Time: 92 ms (5.02%) | Memory: 24.8 MB (5.04%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        vector<int> toffee;
         int n = ratings.size();
-        vector<int> left(n,1);
-        vector<int> right(n,1);
+        int ans = n;
         for(int i=0;i<n;i++){
-            if(i-1>=0&&ratings[i]>ratings[i-1]){
-                left[i] = left[i-1]+1;
+            pair<int,int> temp = {ratings[i],i};
+            pq.push(temp);
+            toffee.push_back(1);
+        }
+        while(!pq.empty()){
+            auto top = pq.top();
+            pq.pop();
+            int rating = top.first;
+            cout<<rating<<" "<<index<<endl;
+            int index = top.second;
+            int left = 0,right = 0;
+            if(index-1>=0&&ratings[index]>ratings[index-1]){
+                left = toffee[index-1];
+            }
+            if(index+1<n&&ratings[index]>ratings[index+1]){
+                right = toffee[index+1];
+            }
+
+            int maxi = max(left,right);
+            if(maxi!=0){
+                ans+=(maxi-toffee[index]+1);
+                toffee[index] = maxi+1;
+
             }
         }
-        for(int i=n-2;i>=0;i--){
-            if(i+1<n&&ratings[i]>ratings[i+1]){
-                right[i] = right[i+1]+1;
-            }
-        }
-        int sum = 0;
-        for(int i=0;i<n;i++){
-            sum+=max(left[i],right[i]);
-        }
-        return sum;
-
-                
-
+        return ans;
     }
 };
