677d463e5271230f8162a35d7b7f01fdb45699e0
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2024-07-18 18:52:19 +0530

    Time: 28 ms (19.10%) | Memory: 26.9 MB (8.48%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,21 +10,53 @@
 
 class Solution {
 public:
+    void preorder(TreeNode* root, vector<TreeNode*>& pre) {
+        if (!root) {
+            return;
+        }
+        pre.push_back(root);
+        preorder(root->left,pre);
+        preorder(root->right,pre);
+    }
+
+    static bool cmp(TreeNode* a, TreeNode* b) { return a->val < b->val; }
+    int find(vector<TreeNode*> vec,int toFind,int ins,int ine){
+        for(int i=ins;i<=ine;i++){
+            if(vec[i]->val==toFind){
+                return i;
+            }
+        }
+        return -1;
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root->val>p->val&&root->val>q->val){
-            return lowestCommonAncestor(root->left,p,q);
-        }
-        if(root->val<p->val&&root->val<q->val){
-            return lowestCommonAncestor(root->right,p,q);
-        }
-        if(root->val==p->val){
-            return p;
-        }
-        if(root->val==q->val){
-            return q;
-        }
-        if((root->val>p->val&&root->val<q->val)||(root->val<p->val&&root->val>q->val)){
-            return root;
+        vector<TreeNode*> preorderV;
+        preorder(root, preorderV);
+        vector<TreeNode*> inorder = preorderV;
+        sort(inorder.begin(), inorder.end(), cmp);
+        int preIndex = 0;
+        int inS = 0;
+        int inE = inorder.size() - 1;
+        pair<int,int> pq;
+        pq.first = find(inorder,p->val,inS,inE);
+        pq.second = find(inorder,q->val,inS,inE);
+        while(inS<=inE){
+            int index = find(inorder,preorderV[preIndex]->val,inS,inE);
+            cout<<pq.first<<index<<pq.second<<endl;
+            if(pq.first==index){
+                return p;
+            }else if(pq.second==index){
+                return q;
+            }
+
+            if((pq.first<index&&pq.second>index)||(pq.second<index&&pq.first>index)){
+                return preorderV[preIndex];
+            }else if((pq.first<index&&pq.second<index)){
+                preIndex++;
+                inE = index-1;
+            }else{
+                preIndex+=index-inS+1;
+                inS = index+1;
+            }
         }
         return NULL;
     }
