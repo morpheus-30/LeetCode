93fccb432f763bfcc7485b954b3a101caf3acd17
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2024-08-04 16:46:11 +0530

    Time: 7 ms (99.25%) | Memory: 28 MB (15.75%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        if(nums.size()==0){
+            return -1;
+        }
+        if(nums.size()<=2){
+            return nums[0];
+        }
+        sort(nums.begin(),nums.end());
+        return nums[(nums.size()/2)];
+    }
+};
