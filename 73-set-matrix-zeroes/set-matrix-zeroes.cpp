4c729fdcb591daa3a2d77dd8591383cc3e01a61c
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2026-03-28 16:23:21 +0530

    Time: 2 ms (19.08%) | Memory: 20.9 MB (10.54%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,49 +1,58 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-         int col0 = 1;
-    // step 1: Traverse the matrix and
-    int n = matrix.size();
-    int m = matrix[0].size();
-    // mark 1st row & col accordingly:
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (matrix[i][j] == 0) {
-                // mark i-th row:
-                matrix[i][0] = 0;
+        int ver = -1;
+        int n = matrix.size();
+        int m = matrix[0].size();
 
-                // mark j-th column:
-                if (j != 0)
-                    matrix[0][j] = 0;
-                else
-                    col0 = 0;
+
+        if(matrix[0][0] == 0){
+            ver = 0;
+        }
+        for(int j=1;j<m;j++){
+            if(matrix[0][j] == 0){
+                matrix[0][0] = 0;
             }
         }
-    }
 
-    // Step 2: Mark with 0 from (1,1) to (n-1, m-1):
-    for (int i = 1; i < n; i++) {
-        for (int j = 1; j < m; j++) {
-            if (matrix[i][j] != 0) {
-                // check for col & row:
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+        for(int i=1;i<n;i++){
+            if(matrix[i][0] == 0){
+                cout<<matrix[i][0]<<endl;
+                ver = 0;
+            }
+        }
+
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][j] == 0){
+                    matrix[0][j] = 0;
+                    matrix[i][0]  = 0;
+                }
+            }
+        }
+
+        //evluating everything other than horizontal and vertical
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){
                     matrix[i][j] = 0;
                 }
             }
         }
-    }
 
-    //step 3: Finally mark the 1st col & then 1st row:
-    if (matrix[0][0] == 0) {
-        for (int j = 0; j < m; j++) {
-            matrix[0][j] = 0;
+        //horizontal
+        if(matrix[0][0] == 0){
+            for(int j=0;j<m;j++){
+                matrix[0][j] = 0;
+            }
         }
-    }
-    if (col0 == 0) {
-        for (int i = 0; i < n; i++) {
-            matrix[i][0] = 0;
+        if(ver == 0){
+            for(int i=0;i<n;i++){
+                matrix[i][0] = 0;
+            }
         }
-    }
 
+        cout<<ver<<matrix[0][0]<<endl;
     }
 };
