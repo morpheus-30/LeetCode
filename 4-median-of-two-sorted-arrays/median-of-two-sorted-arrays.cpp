55cacdc35d9154f39bed07010fbaefda10ab6f09
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2026-04-13 23:45:16 +0530

    Time: 0 ms (100.00%) | Memory: 95 MB (79.46%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        if(n2<n1){
-            return findMedianSortedArrays(nums2,nums1);
-        }
-        int low = 0;
-        int high = n1;
-        int left = (n1+n2+1)/2;
-        while(low<=high){
-            int l1 = INT_MIN,l2 = INT_MIN;
-            int r1 = INT_MAX,r2 = INT_MAX;
-            
-            int mid1 = (low+high)/2;
-            int mid2 = left-mid1;
-            if(mid1<n1) r1 = nums1[mid1];
-            if(mid2<n2) r2 = nums2[mid2];
-            if(mid1-1>=0) l1 = nums1[mid1-1];
-            if(mid2-1>=0) l2 = nums2[mid2-1];
 
-            if(l1<=r2&&l2<=r1){
-                if((n1+n2)%2==0){
-                    double med = (max(l1,l2)+min(r1,r2))/2.0;
-                    return med;
+        int m = nums1.size();
+        int n = nums2.size();
+
+        int req = (m+n+1)/2;
+
+        int low = max(0,req-n);
+        int high = min(req,m);
+
+        while(low<=high){
+            int rfA = low + (high-low)/2;
+            int rfB = req-rfA;
+
+            int leftA  = (rfA == 0) ? INT_MIN : nums1[rfA-1];
+            int rightA = (rfA == m) ? INT_MAX : nums1[rfA];
+
+            int leftB  = (rfB == 0) ? INT_MIN : nums2[rfB-1];
+            int rightB = (rfB == n) ? INT_MAX : nums2[rfB];
+
+            if(leftA <= rightB && leftB <= rightA){
+                if((m+n)%2==0){
+                    return (max(leftA,leftB) + min(rightA,rightB)) / 2.0;
                 }else{
-                    double med = max(l1,l2);
-                    return med;
+                    return max(leftA,leftB);
                 }
-            }else if(l1>r2){
-                high = mid1-1;
-            }else low = mid1+1;
+            }
+            else if(leftA > rightB){
+                high = rfA - 1;
+            }
+            else{
+                low = rfA + 1;
+            }
         }
-        return 0;
+        return -1;
     }
 };
