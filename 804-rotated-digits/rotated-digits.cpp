90975e33ae9b0a362a05e8ae2eae12f0b4aec7c6
Nakshatr-ayo <konnichiwasekai7@gmail.com>
2026-05-02 19:01:05 +0530

    Time: 2 ms (40.88%) | Memory: 9.7 MB (18.77%) - LeetSync

--- 804-rotated-digits/rotated-digits.cpp
+++ 804-rotated-digits/rotated-digits.cpp
@@ -1,30 +1,43 @@
 class Solution {
 public:
+    int mp[10] = {0, 1, 5, -1, -1, 2, 9, -1, 8, 6};
+    int isGood(int num,vector<int> &dp) {
+        if(dp[num] != -1){
+            return dp[num];
+        }
+        if (num == 0) {
+            return dp[num] = 0;
+        }
+        int remain = isGood(num/10,dp);
+        int dig_chk = -1;
+        int rem = num % 10;
+        if(mp[rem] == -1){
+            dig_chk = 2;
+        }else if(mp[rem] != rem){
+            dig_chk = 1;
+        }else if(mp[rem] == rem){
+            dig_chk = 0;
+        }
+
+        if(remain == 0 && dig_chk == 0){
+            return dp[num] = 0;
+        }else if(remain == 2 || dig_chk == 2){
+            return dp[num] = 2;
+        }else{
+            return dp[num] = 1;
+        }
+    }
+
     int rotatedDigits(int n) {
-        int mp[10] = {0,1,5,-1,-1,2,9,-1,8,6};
+        
+        vector<int> dp(n + 1, -1);
 
         int count = 0;
-        for(int i=1;i<=n;i++){
-            int num = i;
-            bool isOtherPresent = false;
-            bool isInvalidPresent = false;
-            while(num>0){
-                int rem = num%10;
-                if(mp[rem] != -1 && mp[rem] != rem){
-                    isOtherPresent = true;
-                }else if(mp[rem] == -1){
-                    isInvalidPresent = true;
-                }
-
-                num = num / 10;
-            }
-
-            if(isOtherPresent && !isInvalidPresent){
+        for (int i = 1; i <= n; i++) {
+            if(isGood(i,dp)==1){
                 count++;
             }
         }
         return count;
-
-
     }
 };
